4196b738f70acaeb5ac2a8b17a0b77502a1e2330
Ванурин Алексей Максимович <avanurin@stream.ru>
2020-05-10 11:59:34 +0300

    fixes

--- MTC-AVHack/Utils/Views/TabBarViewController.swift
+++ MTC-AVHack/Utils/Views/TabBarViewController.swift
@@ -13,15 +13,23 @@ final class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let appearance = UITabBarItem.appearance()
+        let attributes = [NSAttributedString.Key.font:UIFont(name: "MTSSans-Regular", size: 10)]
+        appearance.setTitleTextAttributes(attributes as [NSAttributedString.Key : Any], for: .normal)
+
+        self.tabBar.tintColor = .red
+        
         let auth = NewsAssembly.build()
         let auth2 = LearnAssembly.build()
         let auth3 = ProfileAssembly.build()
         let auth4 = AuthorizeAssembly.build()
         
-        auth.tabBarItem = UITabBarItem(title: "Новости", image: nil, selectedImage: nil)
-        auth2.tabBarItem = UITabBarItem(title: "Обучение", image: nil, selectedImage: nil)
-        auth3.tabBarItem = UITabBarItem(title: "Профиль", image: nil, selectedImage: nil)
-        auth4.tabBarItem = UITabBarItem(title: "Еще", image: nil, selectedImage: nil)
+        auth.tabBarItem = UITabBarItem(title: "Новости", image: #imageLiteral(resourceName: "newspaper 3"), selectedImage: nil)
+        auth2.tabBarItem = UITabBarItem(title: "Обучение", image: #imageLiteral(resourceName: "study"), selectedImage: nil)
+        auth3.tabBarItem = UITabBarItem(title: "Профиль", image: #imageLiteral(resourceName: "man 1"), selectedImage: nil)
+        auth4.tabBarItem = UITabBarItem(title: "Еще", image: #imageLiteral(resourceName: "Group 28"), selectedImage: nil)
+        
+        //auth.font
         
         let tabBarList = [auth, auth2, auth3, auth4]
         
